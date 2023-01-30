2e7d35fe84309bb8f7ae22732228aa415d1ff5fa
grassfinn <theharper@live.com>
2023-01-29 22:04:05 -0500

    Random skyscraper function

--- Skylines/index.js
+++ Skylines/index.js
@@ -6,8 +6,9 @@ import {
     AxesHelper,
     GridHelper,
     CubeTextureLoader,
-    sRGBEncoding
+    sRGBEncoding,
 } from 'three';
+import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as dat from 'dat.gui';
 import { SkyLineSpotLight } from './components/SkyLineSpotLight';
@@ -51,8 +52,42 @@ const ambientLight = new AmbientLight(0x333333);
 
 const skyLineSpotLight = new SkyLineSpotLight();
 
+//! randomly create skyscrapers
+const buildingArr = [];
+function createSkyScrapers(amount = 5) {
+    console.log(typeof amount);
+    if (typeof amount !== 'number') {
+        alert('Something went wrong, amount is supposed to be a number');
+        return 1;
+    }
+    for (let i = 0; i < amount; i++) {
+        // Creating random numbers for the x,y,z
+        const x = THREE.MathUtils.randFloatSpread(20);
+        const y = THREE.MathUtils.randFloatSpread(350);
+        const z = THREE.MathUtils.randFloatSpread(10);
+
+        const buildingGeo = new THREE.BoxGeometry(x, y, z);
+        const buildingMesh = new THREE.MeshBasicMaterial({
+            color: 0x0000ff,
+            wireframe: true,
+        });
+        const building = new THREE.Mesh(buildingGeo, buildingMesh);
+
+        building.position.set(x, y, z);
+        scene.add(building);
+        buildingArr.push(building);
+        console.log(building.position.x);
+        // need to be able to figure out the x and y position of the plane so we can place the building randomly on the plane
+    }
+    // ? I returned the building array in case we can do something with that data
+    return buildingArr;
+}
+
+createSkyScrapers();
+console.log(buildingArr);
+
 const plane = new Plane();
-const background = new Plane(35,13,'vertical',fuji);
+const background = new Plane(35, 13, 'vertical', fuji);
 background.translateZ(17.5);
 background.translateY(6.5);
 
